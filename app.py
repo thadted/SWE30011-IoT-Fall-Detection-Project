c005a234a2e0d5abffc1fbdb4c3b00a8ad25d4d9
Wilson1684 <Wilson336933@gmail.com>
2024-05-27 11:27:22 +0800

    .

--- app.py
+++ app.py
@@ -40,8 +40,7 @@ def fetch_status():
     db_connection.close()
     return status_data
 
-
-def fetch_smartdoor():
+def fetch_smartdoor_data():
     db_connection = connect_to_database()
     cursor = db_connection.cursor(dictionary=True)
     cursor.execute("SELECT * FROM access_logs ORDER BY timestamp DESC LIMIT 1")
@@ -49,9 +48,15 @@ def fetch_smartdoor():
     db_connection.close()
     return status_data
 
+def fetch_smartdoor_status():
+    db_connection = connect_to_database()
+    cursor = db_connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM status_logs ORDER BY timestamp DESC LIMIT 1")
+    status_data = cursor.fetchone()
+    db_connection.close()
+    return status_data
+
 # # Function to fetch threshold settings from the database
-
-
 def fetch_thresholds():
     db_connection = connect_to_database()
     cursor = db_connection.cursor(dictionary=True)
@@ -61,8 +66,6 @@ def fetch_thresholds():
     return thresholds
 
 # # Function to save threshold value
-
-
 def save_threshold(name, value):
     db_connection = connect_to_database()
     cursor = db_connection.cursor()
@@ -180,7 +183,7 @@ def smartband():
 
 @app.route('/smartdoor')
 def smartdoor():
-    status_data = fetch_smartdoor()
+    status_data = fetch_smartdoor_data()
     return render_template('smartdoor.html', status_data=status_data)
 
 
@@ -299,18 +302,26 @@ def get_data():
     else:
         return jsonify({'error': 'No data available'})
 
-
-@app.route('/data2')
+@app.route('/smartdoor_data')
 def get_smartdoor_data():
-    data = fetch_smartdoor()
+    data = fetch_smartdoor_data()
     if data:
         rfid = data.get('rfid')
         message = data.get('message')
-        timestamp = data.get('timestamp')
-        return jsonify({'rfid': rfid, 'message': message, 'timestamp': timestamp})
+        return jsonify({'rfid': rfid, 'message': message})
     else:
         return jsonify({'error': 'No data available'})
-
+    
+@app.route('/smartdoor_status')
+def get_smartdoor_data():
+    data = fetch_smartdoor_status()
+    if data:
+        door_status = data.get('door_status')
+        led_status = data.get('led_status')
+        timestamp = data.get('timestamp')
+        return jsonify({'door_status': door_status, 'led_status': led_status, 'timestamp': timestamp})
+    else:
+        return jsonify({'error': 'No data available'})
 
 @app.route('/settings')
 def settings():
