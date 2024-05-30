a5946febae083cc32613410c83409543d683ee24
Wilson1684 <Wilson336933@gmail.com>
2024-05-30 20:48:19 +0800

    .

--- app.py
+++ app.py
@@ -606,6 +606,16 @@ def get_location_data():
     db_connection.close()
     return data
 
+#location analyze
+def get_time_data():
+    db_connection = connect_to_database()
+    cursor = db_connection.cursor(dictionary=True)
+    cursor.execute(
+        'SELECT * FROM access_logs WHERE timestamp >= NOW() - INTERVAL 1 HOUR')
+    data = cursor.fetchall()
+    db_connection.close()
+    return data
+
 
 @app.route('/insights')
 def insights():
@@ -634,8 +644,16 @@ def insights():
         for data in location_data
     ]
 
+    time_data = get_time_data()
+
+     # Format location data for the graph
+    formatted_time_data = [
+        {'timestamp': data['timestamp'].strftime('%Y-%m-%d %H:%M:%S'), 'location_type': data['location_type']}
+        for data in time_data
+    ]
+
     # Render the insights template with the activity metrics, activity data, recommendations, and location analysis data
-    return render_template('insights.html', activity_metrics=activity_metrics, activity_data=activity_data, recommendations=recommendations, location_data=location_data, formatted_location_data=formatted_location_data)
+    return render_template('insights.html', activity_metrics=activity_metrics, activity_data=activity_data, recommendations=recommendations, location_data=location_data, formatted_location_data=formatted_location_data, time_data=time_data, formatted_time_data=formatted_time_data)
 
 
 #Smartband
